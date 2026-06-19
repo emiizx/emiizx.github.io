675b3ac838c8c0c6b9025c55e1ad0b93387442e7
Aurora X <dannabb19@gmail.com>
2026-06-19 10:29:49 -0500

    Add about page with basic HTML structure

---
+++ pages/about.js
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+
+<title>Acerca de</title>
+
+</head>
+
+<body>
+
+<h1>Aurora Maps</h1>
+
+<p>
+
+Mapa 3D futurista basado en
+MapLibre y OpenStreetMap.
+
+</p>
+
+</body>
+
+</html>
